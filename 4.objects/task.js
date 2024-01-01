58d27c02341d7640128b901ce96de60502c21913
Vitaliy <Groteckon@mail.ru>
2024-01-01 13:26:51 +0300

    Решение задачи

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;  
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;  
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+        this.marks.push(...marks);
+      } else {
+        console.log ("Студент отчислен");
+        return;   
+      }  
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks === 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0);
+    }  
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
